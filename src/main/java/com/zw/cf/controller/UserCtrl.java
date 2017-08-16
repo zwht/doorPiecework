228915f3847a1ebc7f522592e2e2933992a347eb
zhaowei <1512763623@qq.com>
2017-08-16 11:36:03 +0800

    bb

--- src/main/java/com/zw/cf/controller/UserCtrl.java
+++ src/main/java/com/zw/cf/controller/UserCtrl.java
@@ -1,8 +1,6 @@
 package com.zw.cf.controller;
 
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
-import com.wordnik.swagger.annotations.ApiParam;
+import com.wordnik.swagger.annotations.*;
 import com.zw.cf.dao.UserMapper;
 import com.zw.cf.model.UserExample;
 import com.zw.cf.model.User;
@@ -11,12 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 import java.util.Date;
 import java.util.List;
 
@@ -38,6 +34,10 @@ public class UserCtrl {
     @ResponseBody
     @RequestMapping(value = "/add")
     @ApiOperation(value="添加用户",httpMethod="POST",notes="添加用户")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "商品信息"),
+        @ApiResponse(code = 201, message = "(token验证失败)",  response=String.class),
+        @ApiResponse(code = 202, message = "(系统错误)",response = String.class)})
+
     public Response add(
             @ApiParam(required=true,value="用户名",name="userName") @RequestParam String userName,
             @ApiParam(required=true,value="密码",name="passWord") @RequestParam String passWord
@@ -59,25 +59,25 @@ public class UserCtrl {
         }
     }
 
-
-    @RequestMapping(value = "/getUserList", method = RequestMethod.GET,consumes = "application/json")
-    public @ResponseBody List<User> getUserList(HttpServletRequest request, HttpServletResponse response){
+    @ResponseBody
+    @RequestMapping(value = "/getUserList", consumes = "application/json")
+    @ApiOperation(value="获取所有用户列表",httpMethod="GET",notes="获取用户")
+    public  List<User> getUserList(){
 
         UserExample user1Example=null;
         return userMapper.selectByExample(user1Example);
     }
 
-    /**登录接口**/
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public @ResponseBody Response login(HttpServletRequest request, HttpServletResponse response){
 
-        String username=request.getParameter("userName");
-        String password=request.getParameter("passWord");
-
-        User user=userMapper.selectByUserName(username);
-
-
-        if(user.getPassword().equals(password)){
+    @ResponseBody
+    @RequestMapping(value = "/login")
+    @ApiOperation(value="登录接口",httpMethod="POST",notes="登录")
+    public  Response login(
+            @ApiParam(required=true,value="用户名",name="userName") @RequestParam String userName,
+            @ApiParam(required=true,value="密码",name="passWord") @RequestParam String passWord
+    ){
+        User user=userMapper.selectByUserName(userName);
+        if(user.getPassword().equals(passWord)){
             return new Response().success(user);
         }else {
             return new Response().failure("用户名密码错误！");
@@ -85,10 +85,12 @@ public class UserCtrl {
     }
 
 
-
-    @RequestMapping(value = "/getUser", method = RequestMethod.GET,consumes = "application/json")
-    public  @ResponseBody Response selectByPrimaryKey(HttpServletRequest request, HttpServletResponse response){
-        String userId=request.getParameter("userId");
+    @ResponseBody
+    @RequestMapping(value = "/getUser",consumes = "application/json")
+    @ApiOperation(value="根据用户userId获取用户信息",httpMethod="GET",notes="获取用户")
+    public   Response selectByPrimaryKey(
+            @ApiParam(required=true,value="用户Id",name="userId") @RequestParam String userId
+    ){
         User user1=userMapper.selectByPrimaryKey(userId);
         return new Response().success(user1);
     }
