ce892dd6504acc33fcbb8872ecfb552df5b84a5a
zhaowei <1512763623@qq.com>
2018-01-20 16:36:59 +0800

    hsd

--- frontend/src/app/work/door/door.component.ts
+++ frontend/src/app/work/door/door.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
-import {FileUploader} from 'ng2-file-upload';
 @Component({
   selector: 'app-door',
   templateUrl: './door.component.html',
