7b5d4f953568329b785d1ab04795c3815c7fe696
zhaowei <1512763623@qq.com>
2018-02-06 21:18:37 +0800

    hhsd

--- cfmy.sql
+++ cfmy.sql
@@ -1,35 +1,50 @@
-/*
- Navicat Premium Data Transfer
+-- MySQL dump 10.13  Distrib 5.7.18, for macos10.12 (x86_64)
+--
+-- Host: localhost    Database: cfmy
+-- ------------------------------------------------------
+-- Server version	5.7.18-log
 
- Source Server         : cfmy
- Source Server Type    : MySQL
- Source Server Version : 50718
- Source Host           : localhost
- Source Database       : cfmy
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
- Target Server Type    : MySQL
- Target Server Version : 50718
- File Encoding         : utf-8
+--
+-- Table structure for table `code`
+--
 
- Date: 02/06/2018 18:18:24 PM
-*/
-
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
---  Table structure for `code`
--- ----------------------------
 DROP TABLE IF EXISTS `code`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `code` (
   `id` varchar(45) NOT NULL,
   `value` int(4) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
---  Table structure for `color`
--- ----------------------------
+--
+-- Dumping data for table `code`
+--
+
+LOCK TABLES `code` WRITE;
+/*!40000 ALTER TABLE `code` DISABLE KEYS */;
+/*!40000 ALTER TABLE `code` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `color`
+--
+
 DROP TABLE IF EXISTS `color`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `color` (
   `id` varchar(45) NOT NULL,
   `name` varchar(45) NOT NULL,
@@ -38,18 +53,25 @@ CREATE TABLE `color` (
   `value` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
---  Records of `color`
--- ----------------------------
-BEGIN;
-INSERT INTO `color` VALUES ('1516790403629', '??', '1509885871184', 'http://ozq3tirki.bkt.clouddn.com/Fkk42fAG8m8smdJrcoM4P8_pSwfm', 'ggf'), ('1517305544600', '???', '1509885871184', 'http://ozq3tirki.bkt.clouddn.com/Fm5-qUgzNffrLN6oPXlHgu3qvgKt', '#c00');
-COMMIT;
+--
+-- Dumping data for table `color`
+--
 
--- ----------------------------
---  Table structure for `corporation`
--- ----------------------------
+LOCK TABLES `color` WRITE;
+/*!40000 ALTER TABLE `color` DISABLE KEYS */;
+INSERT INTO `color` VALUES ('1516790403629','白色','1509885871184','http://ozq3tirki.bkt.clouddn.com/Fkk42fAG8m8smdJrcoM4P8_pSwfm','ggf'),('1517305544600','大红色','1509885871184','http://ozq3tirki.bkt.clouddn.com/Fm5-qUgzNffrLN6oPXlHgu3qvgKt','#c00');
+/*!40000 ALTER TABLE `color` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `corporation`
+--
+
 DROP TABLE IF EXISTS `corporation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `corporation` (
   `id` varchar(45) NOT NULL,
   `name` varchar(45) NOT NULL,
@@ -60,18 +82,25 @@ CREATE TABLE `corporation` (
   `img` varchar(240) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
---  Records of `corporation`
--- ----------------------------
-BEGIN;
-INSERT INTO `corporation` VALUES ('1509885871184', '????erersdsdds', '??', '0', null, null, 'http://ozq3tirki.bkt.clouddn.com/Ful5IWUDjRgh0NEIA7s87IDEacbZ');
-COMMIT;
+--
+-- Dumping data for table `corporation`
+--
 
--- ----------------------------
---  Table structure for `door`
--- ----------------------------
+LOCK TABLES `corporation` WRITE;
+/*!40000 ALTER TABLE `corporation` DISABLE KEYS */;
+INSERT INTO `corporation` VALUES ('1509885871184','重庆川峰erersdsdds','重庆',0,NULL,NULL,'http://ozq3tirki.bkt.clouddn.com/Ful5IWUDjRgh0NEIA7s87IDEacbZ');
+/*!40000 ALTER TABLE `corporation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `door`
+--
+
 DROP TABLE IF EXISTS `door`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `door` (
   `id` varchar(45) NOT NULL,
   `name` varchar(45) NOT NULL,
@@ -86,29 +115,49 @@ CREATE TABLE `door` (
   `line` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
---  Records of `door`
--- ----------------------------
-BEGIN;
-INSERT INTO `door` VALUES ('1516950198184', '??? _??sddsdssdffsffddf', 'http://ozq3tirki.bkt.clouddn.com/FkugQQ-ySDCyVO2YKCg1OaeeaXfg', '1509885871184', '1515568229765,1515568294692,1515568306490,1515568321123,1515568337743,1515568350142,1515568360154,1515568368535,1515568376630,1515568385260,1515568393850,1515568401028,1515568408065,1515568418392,1515568428501,1515568441272,1515568455633,1515568463485,1515568472353', '2018-01-26 15:03:18', '0', '1515568229765,1515568294692,1515568306490,1515568321123,1515568337743,1515568350142,1515568360154,1515568368535,1515568376630,1515568385260,1515568393850,1515568401028,1515568408065,1515568418392,1515568428501,1515568441272,1515568455633,1515568463485,1515568472353', 'HM_001', 'err', null), ('1516950264960', '????_dhd', 'http://ozq3tirki.bkt.clouddn.com/FusWpSH8vkOeK0EYtAFxfKMEBrp8', '1509885871184', '1515568229765,1515568294692,1515568306490,1515568321123,1515568337743,1515568350142,1515568360154,1515568368535,1515568376630,1515568385260,1515568393850,1515568401028,1515568408065,1515568418392,1515568428501,1515568441272,1515568455633,1515568463485,1515568472353', '2018-01-26 15:04:25', '0', '1515568229765,1515568294692,1515568306490,1515568321123,1515568337743,1515568350142,1515568360154,1515568368535,1515568376630,1515568385260,1515568393850,1515568401028,1515568408065,1515568418392,1515568428501,1515568441272,1515568455633,1515568463485,1515568472353', 'HT_002', 'err', null);
-COMMIT;
+--
+-- Dumping data for table `door`
+--
 
--- ----------------------------
---  Table structure for `errCode`
--- ----------------------------
+LOCK TABLES `door` WRITE;
+/*!40000 ALTER TABLE `door` DISABLE KEYS */;
+INSERT INTO `door` VALUES ('1516950198184','红木门 _活动sddsdssdffsffddf','http://ozq3tirki.bkt.clouddn.com/FkugQQ-ySDCyVO2YKCg1OaeeaXfg','1509885871184','1515568229765,1515568294692,1515568306490,1515568321123,1515568337743,1515568350142,1515568360154,1515568368535,1515568376630,1515568385260,1515568393850,1515568401028,1515568408065,1515568418392,1515568428501,1515568441272,1515568455633,1515568463485,1515568472353','2018-01-26 15:03:18',0,'1515568229765,1515568294692,1515568306490,1515568321123,1515568337743,1515568350142,1515568360154,1515568368535,1515568376630,1515568385260,1515568393850,1515568401028,1515568408065,1515568418392,1515568428501,1515568441272,1515568455633,1515568463485,1515568472353','HM_001','err',NULL),('1516950264960','黑桃木门_dhd','http://ozq3tirki.bkt.clouddn.com/FusWpSH8vkOeK0EYtAFxfKMEBrp8','1509885871184','1515568229765,1515568294692,1515568306490,1515568321123,1515568337743,1515568350142,1515568360154,1515568368535,1515568376630,1515568385260,1515568393850,1515568401028,1515568408065,1515568418392,1515568428501,1515568441272,1515568455633,1515568463485,1515568472353','2018-01-26 15:04:25',0,'1515568229765,1515568294692,1515568306490,1515568321123,1515568337743,1515568350142,1515568360154,1515568368535,1515568376630,1515568385260,1515568393850,1515568401028,1515568408065,1515568418392,1515568428501,1515568441272,1515568455633,1515568463485,1515568472353','HT_002','err',NULL);
+/*!40000 ALTER TABLE `door` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `errCode`
+--
+
 DROP TABLE IF EXISTS `errCode`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `errCode` (
   `id` varchar(45) NOT NULL,
   `value` int(4) NOT NULL,
   `label` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
---  Table structure for `gx`
--- ----------------------------
+--
+-- Dumping data for table `errCode`
+--
+
+LOCK TABLES `errCode` WRITE;
+/*!40000 ALTER TABLE `errCode` DISABLE KEYS */;
+/*!40000 ALTER TABLE `errCode` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `gx`
+--
+
 DROP TABLE IF EXISTS `gx`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `gx` (
   `id` varchar(45) NOT NULL,
   `type` int(4) NOT NULL,
@@ -119,18 +168,25 @@ CREATE TABLE `gx` (
   `serial` int(4) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
---  Records of `gx`
--- ----------------------------
-BEGIN;
-INSERT INTO `gx` VALUES ('1515568229765', '1', '12', '1509885871184', '??', '0', '1'), ('1515568294692', '2', '11', '1509885871184', '??', '0', '2'), ('1515568306490', '12', '22', '1509885871184', '??', '0', '3'), ('1515568321123', '33', '12', '1509885871184', '???', '0', '4'), ('1515568337743', '33', '12', '1509885871184', '??', '0', '5'), ('1515568350142', '3', '2', '1509885871184', '??', '0', '6'), ('1515568360154', '3', '2', '1509885871184', '??', '0', '7'), ('1515568368535', '3', '2', '1509885871184', '??', '0', '8'), ('1515568376630', '3', '23', '1509885871184', '??', '0', '9'), ('1515568385260', '3', '23', '1509885871184', '??', '0', '10'), ('1515568393850', '3', '23', '1509885871184', '????', '0', '11'), ('1515568401028', '3', '23', '1509885871184', '??', '0', '12'), ('1515568408065', '3', '2', '1509885871184', '??', '0', '13'), ('1515568418392', '3', '2', '1509885871184', '??', '0', '14'), ('1515568428501', '3', '23', '1509885871184', '??', '0', '15'), ('1515568441272', '3', '23', '1509885871184', '??', '0', '16'), ('1515568455633', '3', '23', '1509885871184', '??', '0', '17'), ('1515568463485', '4', '23', '1509885871184', '??', '0', '18'), ('1515568472353', '3', '23', '1509885871184', '??', '0', '19');
-COMMIT;
+--
+-- Dumping data for table `gx`
+--
 
--- ----------------------------
---  Table structure for `line`
--- ----------------------------
+LOCK TABLES `gx` WRITE;
+/*!40000 ALTER TABLE `gx` DISABLE KEYS */;
+INSERT INTO `gx` VALUES ('1515568229765',1,12,'1509885871184','下料',0,1),('1515568294692',2,11,'1509885871184','单套',0,2),('1515568306490',12,22,'1509885871184','雕刻',0,3),('1515568321123',33,12,'1509885871184','抓架子',0,4),('1515568337743',33,12,'1509885871184','压机',0,5),('1515568350142',3,2,'1509885871184','精裁',0,6),('1515568360154',3,2,'1509885871184','打台',0,7),('1515568368535',3,2,'1509885871184','封边',0,8),('1515568376630',3,23,'1509885871184','大沙',0,9),('1515568385260',3,23,'1509885871184','异型',0,10),('1515568393850',3,23,'1509885871184','打眼订凳',0,11),('1515568401028',3,23,'1509885871184','分货',0,12),('1515568408065',3,2,'1509885871184','刮沙',0,13),('1515568418392',3,2,'1509885871184','贴纸',0,14),('1515568428501',3,23,'1509885871184','贴皮',0,15),('1515568441272',3,23,'1509885871184','底漆',0,16),('1515568455633',3,23,'1509885871184','油磨',0,17),('1515568463485',4,23,'1509885871184','面漆',0,18),('1515568472353',3,23,'1509885871184','包装',0,19);
+/*!40000 ALTER TABLE `gx` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `line`
+--
+
 DROP TABLE IF EXISTS `line`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `line` (
   `id` varchar(45) NOT NULL,
   `name` varchar(45) NOT NULL,
@@ -139,18 +195,25 @@ CREATE TABLE `line` (
   `value` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
---  Records of `line`
--- ----------------------------
-BEGIN;
-INSERT INTO `line` VALUES ('1516338101368', '????', '1509885871184', 'http://ozq3tirki.bkt.clouddn.com/Fjk7A9r1Ucpp2Cfh8oqBtMKhZthI', '323'), ('1517305642143', '???', '1509885871184', 'http://ozq3tirki.bkt.clouddn.com/FtTnwAohR1ioyUNaf9F4wfXDvahB', null);
-COMMIT;
+--
+-- Dumping data for table `line`
+--
 
--- ----------------------------
---  Table structure for `process`
--- ----------------------------
+LOCK TABLES `line` WRITE;
+/*!40000 ALTER TABLE `line` DISABLE KEYS */;
+INSERT INTO `line` VALUES ('1516338101368','花纹线条','1509885871184','http://ozq3tirki.bkt.clouddn.com/Fjk7A9r1Ucpp2Cfh8oqBtMKhZthI','323'),('1517305642143','大条线','1509885871184','http://ozq3tirki.bkt.clouddn.com/FtTnwAohR1ioyUNaf9F4wfXDvahB',NULL);
+/*!40000 ALTER TABLE `line` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `process`
+--
+
 DROP TABLE IF EXISTS `process`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `process` (
   `id` varchar(45) NOT NULL,
   `gx_id` varchar(45) DEFAULT NULL,
@@ -165,11 +228,24 @@ CREATE TABLE `process` (
   `ticket_id` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
---  Table structure for `product`
--- ----------------------------
+--
+-- Dumping data for table `process`
+--
+
+LOCK TABLES `process` WRITE;
+/*!40000 ALTER TABLE `process` DISABLE KEYS */;
+/*!40000 ALTER TABLE `process` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
 DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `product` (
   `id` varchar(45) CHARACTER SET latin1 NOT NULL,
   `name` varchar(45) CHARACTER SET latin1 DEFAULT NULL,
@@ -203,11 +279,24 @@ CREATE TABLE `product` (
   `ticket_id` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
---  Table structure for `ticket`
--- ----------------------------
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ticket`
+--
+
 DROP TABLE IF EXISTS `ticket`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ticket` (
   `id` varchar(45) CHARACTER SET latin1 NOT NULL,
   `name` varchar(45) CHARACTER SET latin1 DEFAULT NULL,
@@ -229,11 +318,24 @@ CREATE TABLE `ticket` (
   `sum_line` float DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
---  Table structure for `users`
--- ----------------------------
+--
+-- Dumping data for table `ticket`
+--
+
+LOCK TABLES `ticket` WRITE;
+/*!40000 ALTER TABLE `ticket` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ticket` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
 DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `id` varchar(45) NOT NULL,
   `name` varchar(45) NOT NULL,
@@ -249,12 +351,25 @@ CREATE TABLE `users` (
   `address` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
---  Records of `users`
--- ----------------------------
-BEGIN;
-INSERT INTO `users` VALUES ('121', '赵伟', 'FOG2ALH9V59HQzuI6NhSkQ==', '', '1', '11', 'zw', '1', '0', '1', '2017-11-14 15:49:53', null), ('1515570349126', '??_??', 'FOG2ALH9V59HQzuI6NhSkQ==', '1509885871184', '1', '12355', '123', '??', '4', null, null, null), ('1515570392717', '???_??', 'FOG2ALH9V59HQzuI6NhSkQ==', '1509885871184', '1', '??', '??', '??', '4', null, null, null), ('1515570409321', '??_??', 'FOG2ALH9V59HQzuI6NhSkQ==', '1509885871184', '1', '??', '??', '??', '4', null, null, null), ('1515570427216', '??_???', 'FOG2ALH9V59HQzuI6NhSkQ==', '1509885871184', '1', '???', '???', '???', '4', null, null, null), ('1515570441526', '??_??', 'FOG2ALH9V59HQzuI6NhSkQ==', '1509885871184', '1', '??', '??', '??', '4', null, null, null), ('1515571650195', '???1', 'FOG2ALH9V59HQzuI6NhSkQ==', '1509885871184', '1', '12', '323', '2', '3', null, null, null), ('1517909490135', '??', 'FOG2ALH9V59HQzuI6NhSkQ==', '1509885871184', '1', '2343', 'wangjun', '0', '2', null, null, '34'), ('cfmy', '???????', 'FOG2ALH9V59HQzuI6NhSkQ==', '1509885871184', '1', '23', 'cfmy', '1', '1', null, null, null), ('cfmy1', '??', 'FOG2ALH9V59HQzuI6NhSkQ==', '1509885871184', '1', '1', 'zhaojing', '1', '2', null, null, null);
-COMMIT;
+--
+-- Dumping data for table `users`
+--
 
-SET FOREIGN_KEY_CHECKS = 1;
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('121','èµµä¼Ÿ','FOG2ALH9V59HQzuI6NhSkQ==','',1,'11','zw','1','0','1','2017-11-14 15:49:53',NULL),('1515570349126','下料_张三','FOG2ALH9V59HQzuI6NhSkQ==','1509885871184',1,'12355','123','下料','4',NULL,NULL,NULL),('1515570392717','赵老大_单套','FOG2ALH9V59HQzuI6NhSkQ==','1509885871184',1,'单套','单套','单套','4',NULL,NULL,NULL),('1515570409321','王五_雕刻','FOG2ALH9V59HQzuI6NhSkQ==','1509885871184',1,'雕刻','雕刻','雕刻','4',NULL,NULL,NULL),('1515570427216','小米_抓架子','FOG2ALH9V59HQzuI6NhSkQ==','1509885871184',1,'抓架子','抓架子','抓架子','4',NULL,NULL,NULL),('1515570441526','小王_压机','FOG2ALH9V59HQzuI6NhSkQ==','1509885871184',1,'压机','压机','压机','4',NULL,NULL,NULL),('1515571650195','经销商1','FOG2ALH9V59HQzuI6NhSkQ==','1509885871184',1,'12','323','2','3',NULL,NULL,NULL),('1517909490135','王君','FOG2ALH9V59HQzuI6NhSkQ==','1509885871184',1,'2343','wangjun','0','2',NULL,NULL,'34'),('cfmy','川峰超级管理员','FOG2ALH9V59HQzuI6NhSkQ==','1509885871184',1,'23','cfmy','1','1',NULL,NULL,NULL),('cfmy1','赵静','FOG2ALH9V59HQzuI6NhSkQ==','1509885871184',1,'1','zhaojing','1','2',NULL,NULL,NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-02-06 21:13:50
