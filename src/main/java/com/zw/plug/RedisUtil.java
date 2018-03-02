a6c2a95a8d629e670ed787b762b47c7fccf9f1c2
zhaowei <1512763623@qq.com>
2018-03-02 18:26:45 +0800

    hhshd

--- src/main/java/com/zw/plug/RedisUtil.java
+++ src/main/java/com/zw/plug/RedisUtil.java
@@ -33,9 +33,9 @@ public class RedisUtil {
             // 最大空闲连接数, 默认8个 控制一个pool最多有多少个状态为idle(空闲的)的jedis实例。
             config.setMaxIdle(8);
             // 最大连接数, 默认8个
-            config.setMaxTotal(200);
+            config.setMaxTotal(20);
             // 表示当borrow(引入)一个jedis实例时，最大的等待时间，如果超过等待时间，则直接抛出JedisConnectionException；
-            config.setMaxWaitMillis(1000 * 100);
+            config.setMaxWaitMillis(1000 * 5);
             // 在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
             config.setTestOnBorrow(true);
             jedisPool = new JedisPool(config, ADDR, PORT, 3000);
--- src/main/java/com/zw/plug/TokenUtil.java
+++ src/main/java/com/zw/plug/TokenUtil.java
@@ -1,5 +1,7 @@
 package com.zw.plug;
 import com.zw.cf.model.User;
+import redis.clients.jedis.Jedis;
+
 import java.util.Date;
 import java.util.List;
 
@@ -9,24 +11,39 @@ import java.util.List;
 public class TokenUtil {
 
     public static void setToken(String id,String token){
-        RedisUtil.getJedis().lpush(id, token);
+        Jedis jedis =RedisUtil.getJedis();
+        try {
+            jedis.lpush(id, token);
+        }catch (Exception e){
+            RedisUtil.close(jedis);
+        }finally{
+            RedisUtil.close(jedis);
+        }
     }
     public static boolean getToken(String token){
         User user = JwtUtils.unsign(token, User.class);
         Boolean key=false;
-        List<String> list=RedisUtil.getJedis().lrange(user.getId(),0,10);
-        for(int i=0; i<list.size(); i++) {
-            User user1 = JwtUtils.unsign(list.get(i), User.class);
-            Date newDate=new Date();
-            Date date=user1.getTokenTime();
-            if(newDate.getTime()<date.getTime()){
-                if(token.equals(list.get(i))){
-                    key=true;
+        Jedis jedis =RedisUtil.getJedis();
+        try{
+            List<String> list=jedis.lrange(user.getId(),0,10);
+            for(int i=0; i<list.size(); i++) {
+                User user1 = JwtUtils.unsign(list.get(i), User.class);
+                Date newDate=new Date();
+                Date date=user1.getTokenTime();
+                if(newDate.getTime()<date.getTime()){
+                    if(token.equals(list.get(i))){
+                        key=true;
+                    }
+                }else{
+                    jedis.rpush(user.getId(),list.get(i));
                 }
-            }else{
-                RedisUtil.getJedis().rpush(user.getId(),list.get(i));
             }
+            return key;
+        }catch (Exception e){
+            RedisUtil.close(jedis);
+            return false;
+        }finally{
+            RedisUtil.close(jedis);
         }
-        return key;
     }
 }
