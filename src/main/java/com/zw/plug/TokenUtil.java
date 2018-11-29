4209f1c0847c36a64992ae1adc89294ba8fb8838
魏翼超然 <1512763623@qq.com>
2018-11-29 21:12:18 +0800

    redis toekn 修改

--- src/main/java/com/zw/plug/TokenUtil.java
+++ src/main/java/com/zw/plug/TokenUtil.java
@@ -1,4 +1,5 @@
 package com.zw.plug;
+
 import com.zw.cf.model.User;
 import redis.clients.jedis.Jedis;
 
@@ -10,40 +11,43 @@ import java.util.List;
  */
 public class TokenUtil {
 
-    public static void setToken(String id,String token){
-        Jedis jedis =RedisUtil.getJedis();
+    public static void setToken(String id, String token) {
+        Jedis jedis = RedisUtil.getJedis();
         try {
             jedis.lpush(id, token);
-        }catch (Exception e){
+        } catch (Exception e) {
             RedisUtil.close(jedis);
-        }finally{
+        } finally {
             RedisUtil.close(jedis);
         }
     }
-    public static boolean getToken(String token){
+
+    public static boolean getToken(String token) {
         User user = JwtUtils.unsign(token, User.class);
-        Boolean key=false;
-        Jedis jedis =RedisUtil.getJedis();
-        try{
-            List<String> list=jedis.lrange(user.getId(),0,10);
-            for(int i=0; i<list.size(); i++) {
+        Boolean key = false;
+        Jedis jedis = RedisUtil.getJedis();
+        try {
+            List<String> list = jedis.lrange(user.getId(), 0, 10);
+            for (int i = 0; i < list.size(); i++) {
                 User user1 = JwtUtils.unsign(list.get(i), User.class);
-                Date newDate=new Date();
-                Date date=user1.getTokenTime();
-                if(newDate.getTime()<date.getTime()){
-                    if(token.equals(list.get(i))){
-                        key=true;
+                if (user1 == null) {
+                    continue;
+                }
+                Date newDate = new Date();
+                Date date = user1.getTokenTime();
+                if (newDate.getTime() < date.getTime()) {
+                    if (token.equals(list.get(i))) {
+                        key = true;
                     }
-                }else{
-                    jedis.rpush(user.getId(),list.get(i));
+                } else {
+                    jedis.lrem(user.getId(), 0, list.get(i));
                 }
             }
+            RedisUtil.close(jedis);
             return key;
-        }catch (Exception e){
+        } catch (Exception e) {
             RedisUtil.close(jedis);
             return false;
-        }finally{
-            RedisUtil.close(jedis);
         }
     }
 }
